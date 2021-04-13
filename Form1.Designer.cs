d1b7fccb1679a8272ca3d0e5f6b3f11912323913
decemberpei <decemberpei@gmail.com>
2021-04-13 12:35:20 +0800

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+using System;
+
+namespace WinEventHook
+{
+    partial class WinEventHook
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_action = new System.Windows.Forms.Button();
+            this.txt_process_name = new System.Windows.Forms.TextBox();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.txt_tip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_action
+            // 
+            this.btn_action.Location = new System.Drawing.Point(30, 31);
+            this.btn_action.Name = "btn_action";
+            this.btn_action.Size = new System.Drawing.Size(175, 52);
+            this.btn_action.TabIndex = 0;
+            this.btn_action.Text = "Start";
+            this.btn_action.UseVisualStyleBackColor = true;
+            this.btn_action.Click += new System.EventHandler(this.btc_action_Click);
+            // 
+            // txt_process_name
+            // 
+            this.txt_process_name.Location = new System.Drawing.Point(458, 37);
+            this.txt_process_name.Name = "txt_process_name";
+            this.txt_process_name.Size = new System.Drawing.Size(338, 42);
+            this.txt_process_name.TabIndex = 1;
+            this.txt_process_name.TextChanged += new System.EventHandler(this.txt_process_name_TextChanged);
+            // 
+            // txt_log
+            // 
+            this.txt_log.Location = new System.Drawing.Point(30, 114);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_log.Size = new System.Drawing.Size(766, 486);
+            this.txt_log.TabIndex = 2;
+            this.txt_log.TextChanged += new System.EventHandler(this.txt_log_TextChanged);
+            // 
+            // txt_tip
+            // 
+            this.txt_tip.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_tip.Enabled = false;
+            this.txt_tip.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txt_tip.Location = new System.Drawing.Point(256, 40);
+            this.txt_tip.Name = "txt_tip";
+            this.txt_tip.ReadOnly = true;
+            this.txt_tip.Size = new System.Drawing.Size(196, 35);
+            this.txt_tip.TabIndex = 3;
+            this.txt_tip.Text = "Process Name:";
+            this.txt_tip.UseWaitCursor = true;
+            // 
+            // WinEventHook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 631);
+            this.Controls.Add(this.txt_tip);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.txt_process_name);
+            this.Controls.Add(this.btn_action);
+            this.Name = "WinEventHook";
+            this.Text = "WinEventHook";
+            this.Load += new System.EventHandler(this.Win_Main_Load);
+            this.Disposed += new System.EventHandler(this.Win_Main_Dispose);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_action;
+        private System.Windows.Forms.TextBox txt_process_name;
+        private System.Windows.Forms.TextBox txt_log;
+        private System.Windows.Forms.TextBox txt_tip;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WinEventHook
+{
+    public partial class WinEventHook : Form
+    {
+        internal delegate void WinEventProc(IntPtr hWinEventHook, int iEvent, IntPtr hWnd, int idObject, int idChild, int dwEventThread, int dwmsEventTime);
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern IntPtr SetWinEventHook(int eventMin, int eventMax, IntPtr hmodWinEventProc, WinEventProc lpfnWinEventProc, int idProcess,
+            int idThread, SetWinEventHookFlags dwflags);
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        private WinEventProc listener = null;
+        private IntPtr hWinEventHook = IntPtr.Zero;
+        private int lastEventTime = 0;
+
+        const int EVENT_SYSTEM_FOREGROUND = 0x0003;
+        const int EVENT_SYSTEM_CONTEXTHELPEND = 0x000D;
+
+        internal enum SetWinEventHookFlags
+        {
+            WINEVENT_OUTOFCONTEXT = 0,
+            WINEVENT_SKIPOWNPROCESS = 2
+        }
+
+        public WinEventHook()
+        {
+            InitializeComponent();
+        }
+
+        private void EventCallback(IntPtr hWinEventHook, int iEvent, IntPtr hWnd, int idObject, int idChild, int dwEventThread, int dwmsEventTime)
+        {
+            string event_string = event_to_str(iEvent);
+            if(event_string != null)
+            {
+                if (lastEventTime == 0)
+                {
+                    txt_log.Text = txt_log.Text + Environment.NewLine + event_string + " at " + dwmsEventTime;
+                }
+                else
+                {
+                    txt_log.Text = txt_log.Text + Environment.NewLine + event_string + " +" + (dwmsEventTime - lastEventTime) + "ms.";
+                }
+            }
+            lastEventTime = dwmsEventTime;
+        }
+
+        private void btc_action_Click(object sender, EventArgs e)
+        {
+            if(hWinEventHook == IntPtr.Zero)
+            {
+                if(listener == null)
+                {
+                    listener = new WinEventProc(EventCallback);
+                }
+
+                List<Process> processes_find = findProcessesByName(txt_process_name.Text);
+                if(processes_find.Count != 1)
+                {
+                    MessageBox.Show("Processes found: " + processes_find.Count);
+                    return;
+                }
+
+                hWinEventHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_CONTEXTHELPEND, IntPtr.Zero, listener, 
+                    processes_find[0].Id, 0,SetWinEventHookFlags.WINEVENT_OUTOFCONTEXT | SetWinEventHookFlags.WINEVENT_SKIPOWNPROCESS);
+                txt_log.Text = "";
+                btn_action.Text = "Stop";
+            }
+            else
+            {
+                UnhookWinEvent(hWinEventHook);
+                hWinEventHook = IntPtr.Zero;
+                btn_action.Text = "Start";
+            }
+        }
+
+        private void txt_process_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_log_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        static string event_to_str(int event_id)
+        {
+            if (event_id != 0x0009 && event_id != 0x0003)
+            {
+                return null;
+            }
+            switch (event_id)
+            {
+                case 0x0003:
+                    return "SYS_FOREGROUND";
+                case 0x0004:
+                    return "SYS_MENUSTART";
+                case 0x0006:
+                    return "SYS_MENUPOPUPSTART";
+                case 0x0007:
+                    return "SYS_MENUPOPUPEND";
+                case 0x0008:
+                    return "SYS_CAPTURESTART";
+                case 0x0009:
+                    return "SYS_CAPTUREEND";
+                case 0x000a:
+                    return "SYS_MOVESIZESTART";
+                case 0x000b:
+                    return "SYS_MOVESIZEEND";
+                case 0x0016:
+                    return "SYS_MINIMIZESTART";
+                default:
+                    return "" + event_id;
+            }
+
+        }
+
+        private void Win_Main_Dispose(object sender, EventArgs e)
+        {
+            if(hWinEventHook != IntPtr.Zero)
+            {
+                UnhookWinEvent(hWinEventHook);
+                hWinEventHook = IntPtr.Zero;
+            }
+        }
+
+        private void Win_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<Process> findProcessesByName(string name)
+        {
+            if (name.EndsWith(".exe"))
+            {
+                name = name.Substring(0, name.Length - 4);
+            }
+            List<Process> processes_find = new List<Process>();
+            Process[] ps = Process.GetProcesses();
+            foreach (Process p in ps)
+            {
+                if (p.ProcessName.Equals(name))
+                {
+                    processes_find.Add(p);
+                }
+            }
+            return processes_find;
+        }
+    }
+}
